b4261abf22fbed6f071ee78be4803231938c2534
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-15 19:29:44 -0400

    Mongoose
    -Created the connection with mongoose with the url

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,8 @@
 // Importing mongoose library
 const mongoose = require('mongoose')
+// Connecting to the MongoDB database using the MongoDB URI provided in the environment 
+// variables or using the default URI if the environment variable is not set
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:27017/Firewolfer',{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
