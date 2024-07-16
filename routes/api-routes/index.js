f77531c892b1b1ac02fe9f08c75b41d0c2b55926
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-15 22:42:04 -0400

    Index
    -Added const for user routes and thought routes
    -defined endpoints
    -export the router

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,4 +1,10 @@
 // Create a router instance
 const router = require('express').Router();
 // Import user and thought routes
-const userROUTES = require('./')
+const userROUTES = require('./user-routes')
+const thoughtRoutes = require('./thought-routes')
+// Define endpoints for user and thought routes
+router.use('/user',userRoutes);
+router.use('/thought',thoughtRoutes);
+// Export the router
+module.exports = router;
