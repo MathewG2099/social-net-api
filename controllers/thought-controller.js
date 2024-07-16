e8c00d3d756fa61153bd7772674331796b9d1fb1
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-15 21:25:22 -0400

    Handelers
    -Created a handeler for the creation of thought
    -Created handeler for dlete a thought
    -Created a Handeler for update the thought by ID
    -Created and added a handeler for the creation of reaction
    -Handeler for delete it

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -25,3 +25,69 @@ const ThoughtController = {
         res.status(500).json(err);
       }
     },
+    // Handler for the "create thought" API endpoint
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.status(201).json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  // Handler for the "delete thought" API endpoint
+  async deleteThought(req,res) {
+    try {
+        const thought = await Thought.findByIdAndDelete({_id:req.params.thoughtId});
+        res.status(200).json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  },
+
+  // Handler for the "update thought by ID" API endpoint
+  async updateThoughtById(req, res) {
+    try {
+      const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, {
+        new: true,
+      });
+      if (!thought) {
+        res.status(404).json({ message: 'Thought not found' });
+      } else {
+        res.json(thought);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Handler for the "create reaction" API endpoint
+  async createReaction(req, res) {
+      try {
+        const thought = await Thought.findOneAndUpdate(
+            {_id:req.params.thoughtId},
+            {$addToSet: {reactions: req.body}},
+            {runValidators: true, new: true}
+        );
+        thought ? res.json(thought) : res.status(404).json({message: notFound});
+    } catch (e) {
+        res.status(500).json(e);
+    }
+  },
+
+// Handler for the "delete reaction" API endpoint
+  async deleteReaction(req, res) {
+      try {
+        const thought = await Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$pull: {reactions: {reactionId: req.params.reactionId}}},
+            {runValidators: true, new: true}
+        );
+
+        thought ? res.json(thought) : res.status(404).json({message: notFound});
+    } catch (e) {
+        res.status(500).json(e);
+    }
+  },
+
+};
