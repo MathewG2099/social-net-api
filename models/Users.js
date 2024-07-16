b220d00f9dfbd7f8faaaafc792f6bfbef69f9b0a
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-15 20:39:37 -0400

    user schema
    - Created the schedma for the users
    -for the future I willl add regular expresion to validate the, email format

--- models/Users.js
+++ models/Users.js
@@ -1,2 +1,11 @@
 // Importing required dependencies from the mongoose library
 const { Schema, model, Types } = require('mongoose'); 
+// Defining the User schema with the required fields and their respective data types
+const userSchema = new Schema(
+    {
+      username: {
+          type: String,
+          required: true,
+          unique: true,
+          trim: true,
+      },
