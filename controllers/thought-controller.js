4f85ec8ace1f96d193e094479fdeb599362d2ba2
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-15 21:22:30 -0400

    Thought control
    -Defined ThoughtController object with multiples api requests
    -Created handeler for get thought by ID

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,27 @@
 const { Thought,User,Reaction } = require('../models')
+const {Types} = require('mongoose');
+
+// Define the ThoughtController object, which contains methods for handling various API requests related to thoughts
+const ThoughtController = {
+    async getAllThoughts(req, res) {
+      try {
+        const thoughts = await Thought.find({});
+        res.json(thoughts);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+  
+    // Handler for the "get thought by ID" API endpoint
+    async getThoughtsById(req, res) {
+      try {
+        const thought = await Thought.findOne({_id:req.params.thoughtId});
+        if (!thought) {
+          res.status(404).json({ message: 'Thought not found' });
+        } else {
+          res.json(thought);
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
