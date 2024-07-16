b727380379baed072aacddb5d9e0b2143f062b94
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-15 20:48:31 -0400

    email form
    -Added an email format for friends and thoughts

--- models/Users.js
+++ models/Users.js
@@ -9,3 +9,28 @@ const userSchema = new Schema(
           unique: true,
           trim: true,
       },
+       // using regular expression to validate email format
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: { 
+          validator: function(v) {
+              return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(v);
+          }
+      }
+    },
+
+    friends:[
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }
+  ],
+    thoughts:[
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+    }
+  ],
+  },
